ee91272046ab124624604433051d66b831314e0b
Jim Grimes <jimgrimes86@gmail.com>
2023-11-16 10:03:57 -0500

    completed code-along

--- lib/scraper.py
+++ lib/scraper.py
@@ -4,3 +4,11 @@ import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+
+doc = BeautifulSoup(html.text, 'html.parser')
+
+headings = doc.select('.heading-50-black.color-black')
+
+for heading in headings:
+    print(heading.contents[0].strip())
+
